ae1c8585d47044fc0ed6986e03b383a3ebd7ba97
NightFuryL <levodesskiy@gmail.com>
2025-12-06 17:45:13 +0200

    HW 06.12.2025 - 13.12.2025

---
+++ Exercise1/CreditCard.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz_Cs_1.Exercise1
+{
+    internal class CreditCard
+    {
+        public string? CardNumber;
+        public string? Owner;
+        public DateTime ExpireDate;
+        public int Pin;
+        public double creditLimit;
+        public double Money;
+
+        public event Action<string,string>? OnDeposit;
+        public event Action<string,string>? OnWithdraw;
+        public event Action<string,string>? OnCreditStart;
+        public event Action<string,string>? OnTargetReached;
+        public event Action<string,string>? OnPinChanged;
+        public event Action<string, string>? OnShowInfo;
+        //public event Action<string>? OnCreateAccount; 
+
+        public double TargetSum = 10000;
+        public CreditCard(string? number, string? owner, DateTime expire, int pin, double limit, double money)
+        {
+            CardNumber = number;
+            Owner = owner;
+            ExpireDate = expire;
+            Pin = pin;
+            creditLimit = limit;
+            Money = money;
+            //OnCreateAccount += myLog;
+            //OnCreateAccount?.Invoke("Created a credit card: " + Info(), "CREATION");
+            myLog("Created a credit card: " + Info(), "CREATION");//Або можна наприклад зробити лог при створенні картки за допомогою івенту OnCreateAccount
+        }
+
+        public void Deposit(double amount)
+        {
+            Money += amount;
+            OnDeposit?.Invoke($"Deposit: +{amount}, balance: {Money}", "DEPOSIT");
+            if (Money >= TargetSum)
+                OnTargetReached?.Invoke($"Achieved amount: {Money}", "TARGET");
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount <= Money)
+            {
+                Money -= amount;
+                OnWithdraw?.Invoke($"Withdrawn {amount}, balance: {Money}", "WITHDRAW");
+            }
+            else
+            {
+                double need = amount - Money;
+                if (need <= creditLimit)
+                {
+                    creditLimit -= need;
+                    Money = 0;
+                    OnCreditStart?.Invoke($"Credit usage started. Withdrawn {amount}. Balance=0, limit={creditLimit}", "CREDIT");
+                }
+            }
+        }
+
+        public void ChangePin(int newPin)
+        {
+            Pin = newPin;
+            OnPinChanged?.Invoke($"PIN changed to {newPin}", "PIN_CHANGE");
+        }
+
+        public string Info()
+        {
+            string info = $"\n\tCard: {CardNumber}, \n\tOwner: {Owner}, \n\tExpire: {ExpireDate.ToShortDateString()}, \n\tBalance: {Money}, \n\tCredit Limit: {creditLimit}";
+            OnShowInfo?.Invoke("INFO" + info, "INFO");
+            return info;
+        }
+
+        public static void myLog(string message,string typeLog)
+        {
+            string type = (message != null && message != "DEFAULT") ? $"{typeLog}" : "";
+            string generelLogMessage = $"[LOG]({type}) - {message}";
+            File.AppendAllBytes("credit_card_log.txt", Encoding.UTF8.GetBytes(generelLogMessage + Environment.NewLine));//ще додав зберігання логу в файл
+            Console.WriteLine(generelLogMessage);
+        }
+        public static void ReadLogs()
+        {
+            if (File.Exists("credit_card_log.txt"))
+            {
+                string[] logEntries = File.ReadAllLines("credit_card_log.txt");
+                foreach (var entry in logEntries)
+                {
+                    Console.WriteLine(entry);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Log file does not exist.");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,27 +1,62 @@
 ﻿using System;
-using dz_Cs_1.Exercise1;
-
-
+using dz_Cs_1.Exercise1;//Файл із классом CreditCard для другого завдання
+//Над другим завданням працював старанно та додав багато всього іншого:) 
+namespace dz_Cs_1;
 class Program
 {
     static void Main()
     {
-        #region HW 29.11.2025 - 06.12.2025
-        //1
-        Book b1 = new Book { Title = "B1", Author = "AB1", Year = 2000, Pages = 150 };
-        b1.Show();
-        b1.Dispose();
-        Book b2 = new Book { Title = "B2", Author = "AB2", Year = 1999, Pages = 300 };
-        b2 = null;
-        GC.Collect();
-        GC.WaitForPendingFinalizers();
-        //2
-        using (Library lib = new Library())
-        {
-            lib.Add(new Book { Title = "Game Of Thrones", Author = "George R. R. Martin", Year = 1996, Pages = 333 });
-            lib.Add(new Book { Title = "Meditations", Author = "Mark Aurelius", Year = 100, Pages = 333 });
-            lib.ShowAll();
-        }
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        #region Exercise 1
+        //1.1 - 1.3
+        Action showTime = () => Console.WriteLine(DateTime.Now.ToLongTimeString());
+        Action showDate = () => Console.WriteLine(DateTime.Now.ToShortDateString());
+        Action showDay = () => Console.WriteLine(DateTime.Now.DayOfWeek);
+        //1.4 - 1.5
+        Func<double, double, double> TriangleArea = (a, h) => (a * h) / 2;
+        Func<double, double, double> RectArea = (a, b) => a * b;
+        //1.6 я його додав щоб був приклад із Predicate бо в завданні його не було
+        Predicate<int> isEven = x => x % 2 == 0;
+
+        showTime();
+        showDate();
+        showDay();
+        Console.WriteLine(TriangleArea(10, 5));
+        Console.WriteLine(RectArea(4, 6));
+        Console.WriteLine(isEven(10));
+        #endregion
+
+        #region Exercise 2
+        var card = new CreditCard(
+            "1234 5678 9999 0000",
+            "Богля Лев",
+            new DateTime(2000, 4, 7),
+            1234,
+            5000,
+            2000
+        );
+        /*Або так можна зробити через лямбда вирази я залишив щоб було декіклька прикладів
+        card.OnDeposit += msg => Console.WriteLine("[Deposit] " + msg);
+        card.OnWithdraw += msg => Console.WriteLine("[Withdraw] " + msg);
+        card.OnCreditStart += msg => Console.WriteLine("[Credit] " + msg);
+        card.OnTargetReached += msg => Console.WriteLine("[Target] " + msg);
+        card.OnPinChanged += msg => Console.WriteLine("[PIN] " + msg);*/
+        //так ще такий приклад через СТАТИЧНИЙ метод класу
+        card.OnDeposit += CreditCard.myLog;
+        card.OnWithdraw += CreditCard.myLog;
+        card.OnCreditStart += CreditCard.myLog;
+        card.OnTargetReached += CreditCard.myLog;
+        card.OnPinChanged += CreditCard.myLog;
+        card.OnShowInfo += CreditCard.myLog;
+        Console.WriteLine("Console: " + card.Info());
+        
+        card.Deposit(3000);
+        card.Withdraw(6000);
+        card.ChangePin(9999);
+
+        Console.WriteLine("Console: " + card.Info());
+        Console.WriteLine("Reading logs:\n");
+        CreditCard.ReadLogs();
         #endregion
     }
 }
